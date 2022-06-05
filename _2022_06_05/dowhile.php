455f9babce0805e09aa89372146f321fa9b28714
Berat Karabulut <53434685+Inqusiter@users.noreply.github.com>
2022-06-05 23:37:28 +0300

    Add files via upload

---
+++ _2022_06_05/dowhile.php
@@ -0,0 +1,13 @@
+<?php
+    $sayi = 1;
+    do {
+        echo "$sayi - Bir kere çalıştım. Çünkü benim görevim bu."
+    } while($sayi > 10) // şart sağlandığı sürece do komutunu çalıştırırım.
+
+    $sayi2 = 1;
+    do {
+        echo "$sayi - Bir kere çalıştım. Çünkü benim görevim bu."
+        $sayi2++;
+    } while($sayi <= 10) // şart sağlandığı sürece do komutunu çalıştırırım.
+
+?>
